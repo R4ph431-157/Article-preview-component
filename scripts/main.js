13c11f055caeb1df088905d35da61d08edef2486
R4PH431 <dyuraphael@gmail.com>
2025-06-27 13:39:23 +0100

    modified code

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
+// Use the dom to get button elements from the webpage and store for use later
 const buttonElement = document.querySelectorAll('.show-social-icons')
 
+
+// Loop through and add a click event for each button with some instructions to run upon click
 buttonElement.forEach((button) => {
   button.addEventListener('click', (event) => {
     const whiteFooter = document.querySelector('.article-footer-info')
@@ -12,6 +15,7 @@ buttonElement.forEach((button) => {
 
     const isHidden = whiteFooter.classList.contains('is-hidden')
 
+    // check if conditions are met upon click and run the set of instructions accordingly
     if(!isHidden) {
       whiteFooter.classList.add('is-hidden')
 
